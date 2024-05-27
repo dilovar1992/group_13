a0c7d1bf48df13e83c6a7e0f9e9925d06e807536
dilovar1992 <dellovar56@gmail.com>
2024-05-27 16:34:51 -0400

    updated the solution task FINRA

--- src/dilovar/Question_2.java
+++ src/dilovar/Question_2.java
@@ -9,13 +9,13 @@ public class Question_2 {
      */
      public static void main(String[] args) {
          for (int i = 1; i <=30 ; i++) {
-            if(i%3==0){
-                System.out.print("FIN ");
+            if(i%3==0 && i % 5 == 0){
+                System.out.print("FINRA ");
             } else if (i%5==0) {
                 System.out.print("RA ");
 
-            } else if (i%3==0 && i%5==0) {
-                System.out.print("FINRA ");
+            } else if (i % 3 == 0 ) {
+                System.out.print("FIN ");
 
             } else {
                 System.out.print(i+" ");
