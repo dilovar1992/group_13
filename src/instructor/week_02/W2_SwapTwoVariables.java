26dca9d8a159a75f69292431d7da4e5b178d023d
OwenEST <owen.constantine@cydeo.com>
2024-05-31 03:24:31 +0300

    First Task Solution and Second Task Questions

---
+++ src/instructor/week_02/W2_SwapTwoVariables.java
@@ -0,0 +1,17 @@
+package instructor.week_02;
+/*
+Numbers – Swap Numbers
+Swap two variables' values without using a third variable.
+
+*/
+public class W2_SwapTwoVariables {
+
+}
+
+
+
+/*
+Numbers – Swap Numbers
+Swap two variables' values without using a third variable.
+
+*/
