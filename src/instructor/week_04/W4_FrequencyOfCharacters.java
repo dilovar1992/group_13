406b57c499b34b25383a31795f7052393b33207d
OwenEST <owen.constantine@cydeo.com>
2024-06-14 02:01:41 +0300

    Third Task Solution and 4. Task Questions

---
+++ src/instructor/week_04/W4_FrequencyOfCharacters.java
@@ -0,0 +1,14 @@
+package instructor.week_04;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+
+public class W4_FrequencyOfCharacters {
+   // Write a return method that can find the frequency of characters
+  //  Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+  //  solution 1:
+
+
+
+}
---
+++ src/instructor/week_04/W4_SameLetters.java
@@ -0,0 +1,14 @@
+package instructor.week_04;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class W4_SameLetters {
+
+   // Write a return method that check if a string is build out of the same letters as another string.
+   // Ex:  same("abc",  "cab"); -> true
+
+
+
+
+}
