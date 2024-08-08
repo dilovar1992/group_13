2e1f3d7d11f95510b76a6a6965b1be8df529a2e4
dilovar1992 <dellovar56@gmail.com>
2024-08-08 12:25:43 -0400

    tasks week7 is solved

---
+++ src/dilovar/FindMaxOnArray.java
@@ -0,0 +1,44 @@
+package dilovar;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FindMaxOnArray {
+    /*
+    Array -- Find Maximum
+Write a method that can find the maximum number from an int Array
+     */
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 8, 9, 7};
+
+        System.out.println(maxNum(nums));
+
+
+        System.out.println(maxNumSpecial(nums));
+
+
+    }
+
+    public static int maxNum(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (max < nums[i]) {
+                max = nums[i];
+            }
+
+        }
+
+        return max;
+    }
+    public static int maxNumSpecial(int[] nums){
+        List<Integer> numlist=new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            numlist.add(nums[i]);
+        }
+        return Collections.max(numlist);
+    }
+
+
+
+}
