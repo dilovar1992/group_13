8046916b8086c3ba3b5c36273789daa83a7ccb8e
ersuamine <amine.ersu@gmail.com>
2024-08-29 21:41:53 -0400

    Week 10 task setup

---
+++ src/amine/week10/W10_ArrayListAscending.java
@@ -0,0 +1,13 @@
+package amine.week10;
+
+/*ArrayList - sorting in ascending
+Write a method that can sort the ArrayList in Ascending order without using
+the sort method*/
+
+
+public class W10_ArrayListAscending {
+    public static void main(String[] args) {
+
+
+
+    }}
---
+++ src/amine/week10/W10_MapSort.java
@@ -0,0 +1,28 @@
+package amine.week10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Map - Sort the map by values
+Write a method that can sort the Map by values.
+ */
+public class W10_MapSort {
+    public static void main(String[] args) {
+        // Creating an empty HashMap
+        Map<String, Integer> map1
+                = new HashMap<String, Integer>();
+        Map<String, Integer> map2 = new HashMap<>();
+
+        map1.put("Amine", 5);
+        map1.put("Zeynep", 8);
+        map1.put("Ziyoda", 8);
+        map1.put("Metehan", 3);
+        map1.put("Dilovar", 7);
+
+
+
+
+
+    }
+}
